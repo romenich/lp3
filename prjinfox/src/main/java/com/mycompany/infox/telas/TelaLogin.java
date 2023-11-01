33a09084cda132ac989c322dd37609bc715fa735
romenich <romenik.chaves@hotmail.com>
2023-11-01 11:03:28 -0300

    Regenerar política de tamanhos

--- prjinfox/src/main/java/com/mycompany/infox/telas/TelaLogin.java
+++ prjinfox/src/main/java/com/mycompany/infox/telas/TelaLogin.java
@@ -101,7 +101,7 @@ public class TelaLogin extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        pack();
+        setSize(new java.awt.Dimension(310, 211));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
